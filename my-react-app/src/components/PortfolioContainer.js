97d37c7e31dd2a42221a8acec7ba9cb37a082aca
Parker Scully <pscully05@gmail.com>
2023-03-02 15:48:03 -0500

    Deleted unnecessary files, added structure to components and pages

---
+++ my-react-app/src/components/PortfolioContainer.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import home from './pages/home';
+import aboutme from './pages/aboutme';
+import contact from './pages/contact';
+import portfolio from './pages/portfolio';
+import resume from './pages/resume';
+import footer from './pages/footer'
